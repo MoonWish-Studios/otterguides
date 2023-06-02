f2d95f4867b1c21ca4f1a78e10313de5e188bc09
Weibo Zhang <theonlyhamstertoh@gmail.com>
2023-06-01 23:27:50 -0700

    added typeform under /form

---
+++ app/form/page.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function page() {
+  return (
+    <div className="overflow-auto">
+      <iframe
+        className="w-screen  h-screen absolute inset-0"
+        src="https://lzju3bxq7zw.typeform.com/to/kp67dExq"
+      ></iframe>
+    </div>
+  )
+}
