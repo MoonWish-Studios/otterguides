2060ec675cda6c7bf2d0f9ad348145492b24fe55
weibo zhang <theonlyhamstertoh@gmail.com>
2023-06-18 14:31:49 -0700

    added app fodler

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: "next/core-web-vitals",
-  rules: {
-    "react/no-unescaped-entities": "off",
-    "@next/next/no-page-custom-font": "off",
-    "@next/next/no-html-link-for-pages": [2, path.join(__dirname, "app")],
-  },
-}
